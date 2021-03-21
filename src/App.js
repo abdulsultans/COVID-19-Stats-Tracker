a0ed78b59c5cab8fc23bd5773811ae2d1e50be9d
Abdul Sultan <78996729+abdulsultans@users.noreply.github.com>
2021-03-21 22:33:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import CountrySelector from "./components/CountrySelector";
+import Foot from "./components/Foot";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Stats from "./components/Stats";
@@ -13,6 +14,7 @@ function App() {
         <h1>WORLD STATISTICS</h1>
         <Stats url="https://covid19.mathdro.id/api" />
         <CountrySelector />
+        <Foot />
       </div>
     </>
   );
